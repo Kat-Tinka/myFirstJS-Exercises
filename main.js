8ad467b2921813a1a45134a9549aeedeb8f5a4f6
Kat-Tinka <karolinathorwarth@gmail.com>
2022-04-03 23:22:00 +0200

    init

---
+++ main.js
@@ -0,0 +1,157 @@
+/*
+var myName = 'Karolina'
+console.log
+*/
+
+/* //either you have : let firstName = "Jonas" OR: let firtsName = "Elisa" -not both!//
+let firstName="Jonas"
+console.log(firstName);
+*/
+
+/* let js = "amazing";
+if (js === "amazing") alert ("Kukurriku! Java Script is Fun!");
+
+40 + 8+ 23- 10;
+console.log (40 + 8+ 23- 10);
+
+console.log(33)
+console.log(40 + 8 + 23 + 10)
+
+let firstName = "Elisa";
+console.log(firstName);
+
+let myFirstJob = ("Babysitter");
+let myCurrentJob = ("Web Developer");
+
+console.log(myFirstJob);
+*/
+
+
+
+
+/* LMS-CODE ACADEMY BERLIN: PROJEC2-SPRINT1-EPIC1:
+JavaScriptt Basics:
+
+exercise1 variable called myName with your name as value:
+*/
+let myName = "Katinka";
+console.log(myName);
+
+/* 
+Exercise 2
+Create a variable called age with a number that is your age. 
+Do not use string quotes for numbers.
+Add a line to print that variable in the console. 
+Save the file and reload the page. You should see your name and your age
+
+
+/*
+this works, but its not allowed in this task to use string quotes for numbers.
+let age = "41";
+console.log(age);
+*/
+
+let age = 41;
+console.log(age);
+
+/*
+Exercise 3
+Create a variable called juliaAge with a value 32. 
+Create another variable called ageDiff and 
+set it to an expression that calculates your age minus Julias's age. 
+Print the value of ageDiff. 
+Save the file and reload the page. 
+You should see your age and the age difference. 
+If you are younger than Julia, you should see a negative number.
+*/
+
+let julliaAge = 32;
+let ageDiff = 41 - julliaAge;
+console.log(ageDiff);
+
+/*
+Writing code with conditionals
+Conditionals are forms used programming to tell the computer to do different things, 
+depending on some test, e.g., "if the user is logged in, say "Hi" else say "Please log in."
+
+The most basic conditional form is if...then...else...
+
+Exercise 4
+Write a conditional that compares the variable with your age with the number 21. 
+It should print either "You are older than 21" or "You are not older than 21", 
+appropriately, depending on your age. 
+Save your JavaScript file and reload the page. 
+Make sure you see the correct message. 
+Try changing your age in the JavaScript file 
+to make sure the other message prints when it should.
+*/
+/*if (age>21) {console.log("You are older than 21")
+    
+} else {
+   console.log("You are not older than 21")
+}
+
+// let age be 77
+if (age>77) {console.log("You are older than 21")
+    
+} else {
+   console.log("You are not older than 21")
+}
+*/
+
+/*
+Exercise 5
+Write a conditional that compares your age with Julia's age. 
+This conditional will need to test if you are older, younger, 
+or the same age, 
+and print, appropriately, either "Julia is older than you", 
+Julia is younger than you", 
+or "You have the same age as Julia".
+
+Save your changes and reload the file.
+*/
+//whats wrong in the code below?
+if (age>julliaAge) {
+    console.log("Julia is younger than you");
+} else if (age == julliaAge) {
+    console.log("You have the same age as Julia");
+} else if (age < julliaAge) {
+    console.log("Julia is older than you");
+}
+
+/*
+JavaScript Array
+Sorting an Array
+Exercise 1
+Create an array with all the names of your class 
+(including mentors). 
+Sort the array alphabetically. 
+Then print the first element of the array in the console. 
+On the next line, 
+print the last element of the array in the console. 
+On the next line, print all the elements of the array in the console 
+(use a "for" loop). 
+Save the file and reload the page. 
+You should see the first element of the sorted array, 
+the last element of the sorted array, 
+and a list of all the elements in order 
+in the array in the console.
+*/
+
+
+
+/*
+
+*/
+
+
+
+
+
+
+
+     
+
+
+
+
